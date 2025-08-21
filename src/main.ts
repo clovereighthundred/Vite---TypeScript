598d3369c06e7de2acb41e5a5c25b87ba9a0436d
Adegbokun Adefela <aadegbokun.2302873@stu.cu.edu.ng>
2025-08-21 16:18:00 +0100

    Ensure dynamic task list rendering

--- src/main.ts
+++ src/main.ts
@@ -3,3 +3,36 @@ import ListItem from './model/ListItem'
 import FullList from './model/FullList'
 import ListTemplate from './templates/ListTemplate'
 
+const initApp = (): void => {
+    const fullList = FullList.instance
+    const template = ListTemplate.instance
+
+    const itemEntryForm = document.getElementById("task-input") as HTMLFormElement
+    itemEntryForm.addEventListener("submit", (event: SubmitEvent): void => {
+        event.preventDefault()
+
+        const input = document.getElementById("task-input-box") as HTMLInputElement
+        const newEntryText: string = input.value.trim()
+        if (!newEntryText.length) return
+
+        const itemId: number = fullList.list.length
+            ? parseInt(fullList.list[fullList.list.length - 1].id) + 1
+            : 1
+
+        const newItem = new ListItem(itemId.toString(), newEntryText)
+        fullList.addItem(newItem)
+        template.render(fullList)
+        input.value = ""
+    })
+
+    const clearItems = document.getElementById("clearTaskBtn") as HTMLButtonElement 
+    clearItems.addEventListener('click', (): void => {
+        fullList.clearList()
+        template.clear()
+    })
+
+    fullList.load()
+    template.render(fullList)
+}
+
+document.addEventListener("DOMContentLoaded", initApp)
